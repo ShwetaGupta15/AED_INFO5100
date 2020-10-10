ea16a4745099657ed020b0fd907f3a7155bcd05a
Shweta Gupta <71230572+ShwetaGupta15@users.noreply.github.com>
2020-10-09 20:03:40 -0700

    Add files via upload

--- Assignments/A4/Array Largest Element/ArrayMax.java
+++ Assignments/A4/Array Largest Element/ArrayMax.java
@@ -4,24 +4,24 @@ import java.util.*;
 
 public class ArrayMax {
 	
-	public static int largestElement(int arrayList[], int k)
+	public static int largestElement(int myArrayList[], int k)
 	{
-		Arrays.sort(arrayList);
-		return arrayList[arrayList.length-k];
+		Arrays.sort(myArrayList);
+		return myArrayList[myArrayList.length-k];
 	}
 
 	public static void main(String[] args) 
 	{
 		// TODO Auto-generated method stub;
-		int arrayList[] = {3,2,3,1,2,4,5,5,6};
+		int myArrayList[] = {3,2,3,1,2,4,5,5,6};
 		System.out.println("The unsorted array is : ");
-		for (int i = 0; i<arrayList.length;i++) 
+		for (int i = 0; i<myArrayList.length;i++) 
 		{
-            System.out.print(+arrayList[i] + " ");
+            System.out.print(+myArrayList[i] + " ");
 		}	
 		System.out.println('\n');
 		int position = 5;
-		int maxElement = largestElement(arrayList, position );
+		int maxElement = largestElement(myArrayList, position );
 		System.out.println("The "+ position +" largest position in the given array : "+ maxElement);
 			
 	}
